22f1f3b4617347962a785883990d4b519ae3dec9
JV Estolas <fortis201@gmail.com>
2015-07-04 12:49:54 -0700

    updated customer registration queries

--- server/controllers/customers.js
+++ server/controllers/customers.js
@@ -1,6 +1,5 @@
 var mongoose = require('mongoose');
 var Customer = mongoose.model('Customer');
-// var current_customer;
 module.exports = (function (app) {
 	return {
 		//CRUD
@@ -32,14 +31,13 @@ module.exports = (function (app) {
 							res.json({err_message: "Cannot register with an existing username"});
 						} else {
 							console.log("creating customer...");
-							var customer = new Customer ({name: req.body.name, createdAt: new Date()});
+							var customer = new Customer ({first_name: req.body.fName, last_name: req.body.lName, email: req.body.email, username: req.body.uName, password: req.body.pw, createdAt: new Date()});
 							customer.save(function (err, result) {
 								if (err) {
 									console.log(err);
 								} else {
 									console.log("creation success!");
 									console.log(result);
-									// current_customer = result;
 									res.json(result);
 								}
 							})					
